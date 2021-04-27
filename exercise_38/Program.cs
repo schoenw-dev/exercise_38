01cede33fb4c0dd1d43668a3200b5e77446fc180
will <wschoenle93@gmail.com>
2021-04-27 16:26:35 -0400

    first commit for exercise 38

---
+++ exercise_38/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace exercise_37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool repeat = true;
+
+            while (repeat)
+            {
+
+                string message = "Please enter 5 numbers and I will calculate your average:";
+                int[] userArray = new int[5];
+
+                double avg = SumOfArray(message, userArray);
+                Console.WriteLine($"The total of your array is {avg}!");
+
+                Console.WriteLine("Would you like to continue? (y|n)");
+                string userAnswer = Console.ReadLine();
+                if (userAnswer == "y")
+                {
+                    repeat = true;
+                }
+                else if (userAnswer == "n")
+                {
+                    Console.WriteLine("Goodbye!");
+                    repeat = false;
+                }
+                else
+                {
+                    Console.WriteLine("error, goodbye!");
+                    repeat = false;
+                }
+            }
+        }
+
+        public static double SumOfArray(string message, int[] userArray)
+        {
+            Console.WriteLine(message);
+            int[] userNum = new int[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                userNum[i] = int.Parse(Console.ReadLine());
+                userArray[i] = userNum[i];
+
+            }
+            int total = userArray[0] + userArray[1] + userArray[2] + userArray[3] + userArray[4];
+
+            double avg = total / userArray.Length;
+            return avg;
+
+        }
+    }
+}
+
